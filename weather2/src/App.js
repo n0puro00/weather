ffd7e04c26d8013b1495285c7136a0d184eab56e
n0puro00 <n0puro00@students.oamk.fi>
2021-02-21 14:42:58 +0200

    joo

---
+++ weather2/src/App.js
@@ -0,0 +1,71 @@
+import React,{useState,useEffect} from 'react'
+
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather?';
+const ICON_URL = 'http://openweathermap.org/img/wn/';
+const API_KEY = 'd5dcc2e295fe8321defdb0877e1e4d92';
+
+export default function Weather({lat,lng}) {
+  const [temp, setTemp] = useState(0);
+  const [speed, setSpeed] = useState(0);
+  const [direction, setDirection] = useState(0);
+  const [description, setDescription] = useState('');
+  const [icon, setIcon] = useState('');
+
+
+return (
+  <div>
+    <h3>Weather on your location</h3>
+    <p>{temp} C&#176;</p>
+    <p>{speed} m/s {direction} degrees</p>
+    <p>{description}</p>
+    <img src={icon} alt=""/>
+  </div>
+) }
+
+
+useEffect(() => {
+  const url = API_URL + 
+  'lat=' + lat + 
+  '&lon' + lng +
+  '&units=metric' + 
+  '&appid=' + API_KEY;
+
+  fetch(url)
+  .then(res => res.json())
+  .then (
+    (result) => {
+      if (result.main != undefined) {
+        setTemp(result.main.temp);
+        setSpeed(result.wind.speed);
+        setDirection(result.wind.deg);
+        setDescription(result.weather.[0].description);
+        setIcon(ICON_URL + result.weather[0].icon + '@2x.png');
+      }
+    else {
+      alert('Could not read weather information')
+    }
+  },(error) => {
+    alert(error);
+  }
+  )
+}, [])
+
+if (isLoading) {
+  return <p>Loading...</p>;
+}
+else {
+  return (
+    <div className="content">
+      <h3>Your position</h3>
+      <p>
+        Position:nbsp;
+        {lat.toFixed(3)},
+        {lng.toFixed(3)}
+      </p>
+      <Weather lat={lat} lng={lng} />
+    </div>
+  );
+}
+}
+
+export default App;
